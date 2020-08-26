fc2a7bd9f1e59e768004d6f996bfafc5ba6193a7
Nicole Kaye Bilon <nkrodriguez.bilon@gmail.com>
2020-08-26 20:04:21 +0800

    fixed multiple comma bug

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -14,13 +14,18 @@
                 $scope.dishesCountMessage = "Please enter data first.";
             } // checks if undefined or only has whitespaces
             else {
+                $scope.blankDishInput = 0;
                 let dishArr = $scope.dishes.split(',').map(function(dish) {
                     return dish.trim();
                 }); // splits input into single word entries + eliminates whitespaces
                 dishArr = dishArr.filter(function (e) {return e}); // removes empty values from the array
                 $scope.dishesCount = dishArr.length;
 
-                if ($scope.dishesCount <= 3) {
+                if ($scope.dishesCount === 0) {
+                    $scope.blankDishInput = 1;
+                    $scope.dishesCountMessage = "Please enter data first.";
+                }
+                else if ($scope.dishesCount > 0 && $scope.dishesCount < 4 ) {
                     $scope.dishesCountMessage = "Enjoy!";
                 }
                 else {
